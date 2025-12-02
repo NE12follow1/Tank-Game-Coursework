80b7fbbca5f5d5713968b327e63896e22d7d98d2
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2025-12-02 21:32:47 +0000

    Implemented StateMachine.cs

--- TeamCoolAndAwsome/Scripts/StateMachine.cs
+++ TeamCoolAndAwsome/Scripts/StateMachine.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<Type, BaseState> states;
+
+    private BaseState currentState;
+
+    public BaseState CurrentState
     {
-        
+        get
+        {
+            return currentState;
+        }
+        private set
+        {
+            currentState = value;
+        }
     }
 
-    // Update is called once per frame
+    public void SetStates(Disctionary<Type, BaseState> states)
+    {
+        this.states = states;
+    }
+
     void Update()
     {
-        
+        if (CurrentState == null)
+        {
+            CurrentState = states.Values.First();
+        }
+        else
+        {
+            var nextState = CurrentState.StateUpdate();
+
+            if (nextState != null && nextState != CurrentState.GetType())
+            {
+                SwitchToState(nextState);
+            }
+        }
+    }
+
+    void SwitchToState(Type nextState)
+    {
+        CurrentState.StateExit();
+        CurrentState = states[nextState];
+        CurrentState.StateEnter();
     }
 }
