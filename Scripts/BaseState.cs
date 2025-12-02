34d96907ac8e81e6f12c45665dc781c7111ac447
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2025-12-02 21:20:51 +0000

    Add files via upload

---
+++ Scripts/BaseState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class BaseState
+{
+    public abstract Type StateUpdate();
+    public abstract Type StateEnter();
+    public abstract Type StateExit();
+}
