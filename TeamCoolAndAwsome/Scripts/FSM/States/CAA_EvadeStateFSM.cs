d5611a1da2d751ece07a393710b76c5ce8668fc3
srguts <nurabdirahim78@gmail.com>
2025-12-11 20:30:14 +0000

    Update CAA_EvadeStateFSM.cs
    Updated health thresholds and improved evasion logic.

--- TeamCoolAndAwsome/Scripts/FSM/States/CAA_EvadeStateFSM.cs
+++ TeamCoolAndAwsome/Scripts/FSM/States/CAA_EvadeStateFSM.cs
@@ -6,15 +6,16 @@ using System.Linq;
 
 public class CAA_EvadeStateFSM : CAA_BaseStateFSM
 {
-    private CAA_SmartTankFSM smartTank;
+    private CAA_SmartTankFSM smartTank;                                                            // reference to the smart tank
+    private GameObject evadeTargetObj;                                                             // target to evade to
 
-    public float lowHealth = 30;                                                                   // below this means stay in evade
-    public float safeHealth = 60;                                                                  // above this means return to patrol
+    public float lowHealth = 25;                                                                   // below this means stay in evade
+    public float safeHealth = 50;                                                                  // above this means return to patrol
     public float evadeDistance = 20;                                                               // how far to run away
-
     public CAA_EvadeStateFSM(CAA_SmartTankFSM smartTank)
     {
-        this.smartTank = smartTank;
+        this.smartTank = smartTank;                                                                // set reference to smart tank
+        evadeTargetObj = new GameObject("EvadeTarget");                                            // create dummy target object to move to
     }
 
     public override Type StateEnter()
@@ -30,7 +31,7 @@ public class CAA_EvadeStateFSM : CAA_BaseStateFSM
             return typeof(CAA_PatrolStateFSM);
         }
 
-        // Still too low health then continue evading
+        // if there are visible enemies then continue to evade
         if (smartTank.VisibleEnemyTanks.Count > 0)                                                                   
         {
             // find closest visible enemy tank
@@ -39,19 +40,25 @@ public class CAA_EvadeStateFSM : CAA_BaseStateFSM
                 .First().Key;
 
             // move away from enemy
-            Vector3 directionAway =                                                                                  
+            Vector3 directionAway =
                 (smartTank.transform.position - closestEnemy.transform.position).normalized;
 
             Vector3 evadeTarget =
                 smartTank.transform.position + directionAway * evadeDistance;
 
-            smartTank.FollowPathToWorldPoint(evadeTarget, 1);
-            smartTank.TurretFaceWorldPoint(closestEnemy.transform.position);
+            // move the dummy target into LOS
+            evadeTargetObj.transform.position = evadeTarget;
+
+            // follow this target 
+            smartTank.FollowPathToWorldPoint(evadeTargetObj, 1);
+
+            // face the enemy 
+            smartTank.TurretFaceWorldPoint(closestEnemy);
 
             return null;
         }
 
-        // If no enemies visible then return to patrol 
+        // If no enemies visible then back to patrol 
         return typeof(CAA_PatrolStateFSM);
     }
 
